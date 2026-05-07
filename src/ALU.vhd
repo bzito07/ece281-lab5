6d398035cb4d2a92b31f2352e5611546c92c0a8c
bzito07 <225265719+bzito07@users.noreply.github.com>
2026-05-07 15:47:29 -0600

    working on alu.vhd

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -41,7 +41,54 @@ end ALU;
 
 architecture Behavioral of ALU is
 
+    component full_adder is
+        port (
+            A     : in std_logic;
+            B     : in std_logic;
+            Cin   : in std_logic;
+            S     : out std_logic;
+            Cout  : out std_logic
+            );
+        end component full_adder;    
+
+    component ripple_adder is
+        Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
+               B : in STD_LOGIC_VECTOR (7 downto 0);
+               Cin : in STD_LOGIC;
+               S : out STD_LOGIC_VECTOR (7 downto 0);
+               Cout : out STD_LOGIC);
+    end component ripple_adder;
+
+    signal w_carry: std_logic;
+    signal w_b: std_logic_vector (7 downto 0);
+    signal w_S: std_logic_vector (7 downto 0);
+
+
+    
 begin
+
+    with i_op(0) select
+        w_b <=  i_B when '0',
+                not i_B when '1';
+
+    ripple_adder_1: ripple_adder
+    port map(
+        A     => i_A,
+        B     => w_b,
+        Cin   => i_op(0),   -- Directly to input here
+        S     => w_S,
+        Cout  => w_carry
+    );
+    
+    with i_op select
+        o_result    <=  w_S when "000",
+                        w_S when "001",
+                        (i_A and i_B) when "010",
+                        (i_A or i_B) when "011",
+                        
+        
+    
+    
 process(i_A, i_B, i_op)
 begin
     if i_op = "000" then
